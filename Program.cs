a6984c722e1a42844d166421a37b4e99c241bcd6
Gil Guereque <gguereque326@gmail.com>
2025-08-27 20:00:33 -0700

    Building skeleton out

--- Program.cs
+++ Program.cs
@@ -2,5 +2,8 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("This will be a Game Data Parser project. It will take in a file by you inputting its name and then parse the data.");
+Console.WriteLine("Enter the name of the file you want to read: ");
+Console.ReadLine();
+
 
 Console.ReadKey();
