d103faaac750b2c2c6439f57bfa90a7a8a60ffc7
Gil Guereque <gguereque326@gmail.com>
2025-08-30 15:02:51 -0700

    Setting up program.cs app flow

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameDataParser.ReadUserInput;
+
 
 Console.WriteLine("This will be a Game Data Parser project. It will take in a file by you inputting its name and then parse the data.");
-Console.WriteLine("Enter the name of the file you want to read: ");
-Console.ReadLine();
+Console.WriteLine("Please enter the name of the file: ");
+
+
 
 
 Console.ReadKey();
