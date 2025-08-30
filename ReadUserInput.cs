24dcf83b567a35f44d96ce9dd6c854f53bc7c62b
Gil Guereque <gguereque326@gmail.com>
2025-08-29 21:23:09 -0700

    Entering file validation logic

--- ReadUserInput.cs
+++ ReadUserInput.cs
@@ -8,9 +8,21 @@ namespace GameDataParser
 {
     public class ReadUserInput
     {
+        public string FileName { get; set; }
+        public string FileExt { get; set; }
+
         public ReadUserInput()
         {
             
+        }
+
+        public void EnterFileName()
+        {
+
+            while (FileExt != ".json") ;
+            Console.WriteLine("Enter the name of the file you want to read: ");
+            FileName = Console.ReadLine();
+            FileExt = Path.GetExtension(FileName);
         }
     }
 }
