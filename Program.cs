b3d7905bf206a81c22d96c11a5634f1aac98c80f
Gil Guereque <gguereque326@gmail.com>
2025-08-30 15:18:54 -0700

    Refactoring program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using GameDataParser.ReadUserInput;
 
 Console.WriteLine("This will be a Game Data Parser project. It will take in a file by you inputting its name and then parse the data.");
-//Console.WriteLine("Please enter the name of the file: ");
+Console.WriteLine("Afterwards, it will print out the json from the file that was read.");
 
 // Create an instance of ReadUserInput to call the non-static method
 ReadUserInput userInput = new ReadUserInput();
