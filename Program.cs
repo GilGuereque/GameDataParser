797fe02e940f2b8840780ecdbf808f5639d91dfe
Gil Guereque <gguereque326@gmail.com>
2025-09-15 20:47:21 -0700

    Updating program.cs comments

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ userInput.EnterFileName();
 
 // TODO: Implement file reading and JSON parsing logic
 // TODO: Need to implement error handling for file reading and JSON parsing
-
+// TODO: Consider logging errors to a file for later review
+// TODO: Will need to print the parsed JSON data to the console in a readable format and to a file possibly
 
 Console.ReadKey();
