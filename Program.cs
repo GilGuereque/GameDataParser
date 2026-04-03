4b68643dd96d7b19546049d65eb90a5be8aefce0
Gil Guereque <gguereque326@gmail.com>
2026-04-02 19:07:02 -0700

    Updated program.cs structure

--- Program.cs
+++ Program.cs
@@ -1,16 +1,37 @@
 ﻿using GameDataParser.ReadUserInput;
 
-Console.WriteLine("This project will be a Game Data parser. It will take in the file that you input and then parse the data within.");
-Console.WriteLine("Afterwards, it will print out the json data from the file that was read.");
+var app = new GameDataParserApp();
 
-// Create an instance of ReadUserInput to call the non-static method
-FileNameReader userInput = new FileNameReader();
-userInput.PromptForFileName();
+try
+{
+    app.Run();
+}
+catch(Exception ex)
+{
+    Console.WriteLine(
+        "Sorry! The application has experience an unexpected error " +
+        "and will have to be closed." +
+        $"{ex.Message}");
+}
+
+Console.WriteLine("Press any key to close the app.");
+Console.ReadKey();
+// End of program
+
+public class GameDataParserApp
+{
+    public void Run()
+    {
+        Console.WriteLine("This project will be a Game Data parser. \nIt will take in the file that you input and then parse the data within.");
+        Console.WriteLine("\nAfterwards, it will print out the json data from the file that was read.\n");
+
+        // Create an instance of ReadUserInput to call the non-static method
+        FileNameReader userInput = new FileNameReader();
+        userInput.PromptForFileName();
+    }
+}
 
 // TODO: Implement file reading and JSON parsing logic
 // TODO: Need to implement error handling for file reading and JSON parsing
 // TODO: Consider logging errors to a file for later review
 // TODO: Will need to print the parsed JSON data to the console in a readable format and to a file possibly
-
-Console.ReadKey();
-// End of program
