86d1a969875c6b2633364972ce1b5a8b77909d8a
Gil Guereque <gguereque326@gmail.com>
2026-04-13 16:49:25 -0700

    Created our own logger.cs class file and implemented logging into the app flow

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
 ﻿using System.Text.Json;
 
+var app = new GameDataParserApp();
+var logger = new Logger("log.txt");
+
+try
+{
+    app.Run();
+}
+catch(Exception ex)
+{
+    Console.WriteLine(
+        "Sorry! The application has experienced an unexpected error " +
+        "and will have to be closed.");
+    logger.Log(ex);
+}
 
 Console.WriteLine("\nPress enter to close the app.");
 Console.ReadKey();
